83a7d7b1e77d39c3b551958a26ac36e7916dea41
bishwascvrc1216@gmail.com <bishwascvrc1216@gmail.com>
2019-05-17 16:23:28 +0530

    first commit

---
+++ 13-05/14-05-2019/main.js
@@ -0,0 +1,20 @@
+// var obj=new Object();
+// obj.name="Rahul",
+// obj.address="BTM",
+// obj.phone=9583971929;
+// console.log(obj);
+
+var Person=function(living,age,gender){
+    this.living=living
+    this.age=age;
+    this.gender=gender
+
+}
+
+
+
+var cody=new Person('BTM',27,'Male');
+console.log(cody);
+
+var mody=new Person('Banaskankri',24,'Female');
+console.log(mody);
---
+++ udemytask.js
@@ -0,0 +1,12 @@
+function callFunction(){
+
+
+    var arr=["Rahul","Vicky","Manish"];
+
+console.log(arr);
+arr.push("")
+
+}
+
+
+callFunction();
